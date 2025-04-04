745ed6d013cc7055ace8097e1848c0813be155e3
[delawhora] <sideloer@calpoly.edu>
2025-04-03 17:23:43 -0700

    first commit

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,40 @@
-public class Lab00
-{
-   public static void main(String[] args)
-   {
+import java.util.Arrays;
 
-   
+public class Lab00 {
+
+   public static void main(String[] args) {
+      // declaring, initializing, & printing variables
+      int x = 5;
+      String y = "hello";
+      float z = 9.8f;
+      System.out.println("x:" + x + "," + " y:" + y + "," + " z:" + z);
+
+      // making an array
+      int[] nums = {3, 6, -1, 2};
+      for (int item : nums) {
+         System.out.println(item);
+      }
+
+      // calling a function
+      int numFound = char_count(y, 'l');
+      System.out.println("FOUND: " + numFound);
+
+      // counting FOR loop
+      for (int i = 1; i < 11; i++) {
+         System.out.print(i + " ");
+      }
+
    }
+   public static int char_count(String s, char c) {
+      int count = 0;
+      for (char i : s.toCharArray()) {
+         if (i == c)
+            count += 1;
+      }
+      return (count);
+
+   }
+
+
+
 }
